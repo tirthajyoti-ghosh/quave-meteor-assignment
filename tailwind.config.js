c56c001ba32761e658b504051f9773df823b7a53
Tirthajyoti Ghosh <itirthahere@gmail.com>
2024-02-19 17:19:55 +0530

    Add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './ui/*.{js,jsx,ts,tsx}',
+    './client/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
